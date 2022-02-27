b52682c7e8716a257efc4164fd31a5732f915813
Krzysztof Witucki <krzysiek.witucki55@gmail.com>
2022-02-27 08:28:31 +0100

    initial commit:   * use spring initializer   * create README file

---
+++ src/main/java/com/web/wit/WitApplication.java
@@ -0,0 +1,13 @@
+package com.web.wit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web/wit/WitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.wit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
