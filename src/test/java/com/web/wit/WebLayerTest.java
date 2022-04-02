2e482bd89b37c167de72a6c83cfea60d8790b6f2
Krzysztof Witucki <krzysiek.witucki55@gmail.com>
2022-04-02 17:26:35 +0200

    add tests for PUT /api/v1/users/{id} * updateUserShouldReturnHTTPOKOnValidUserUpdate * updateUserShouldReturnHTTPOKWhenUserIDIsNotInBodyAndRestIsValid
    create utility methods for:
    * creating new user
    * converting object to json string

--- src/test/java/com/web/wit/WebLayerTest.java
+++ src/test/java/com/web/wit/WebLayerTest.java
@@ -35,6 +35,33 @@ public class WebLayerTest {
         userRepository.deleteAll();
     }
 
+
+    /* Utility function for creating new User.
+    * Simple POST request.
+    * This method expects HTTP 201 so any other response is considered as a failure
+    * and will result in thrown Exception.
+    */
+    void createUser(User user) throws Exception{
+        /* Convert to JSON */
+        String json = objectToJson(user);
+
+        /* Send POST request to create user */
+        MvcResult result = this.mockMvc.perform(post("/api/v1/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json)).andReturn();
+
+        int resultStatus = result.getResponse().getStatus();
+        if(resultStatus != 201){
+            throw new Exception("User could not be created. Response code: " + resultStatus);
+        }
+    }
+
+    String objectToJson(Object obj) throws Exception{
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+
+        return ow.writeValueAsString(obj);
+    }
+
     @Test
     void getUsersEndpointShouldReturnHTTP200StatusAndJsonTypeResponse() throws Exception {
         this.mockMvc.perform(get("/api/v1/users/"))
@@ -65,15 +92,8 @@ public class WebLayerTest {
         ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
         String json = ow.writeValueAsString(user);
 
-        /*
-         * First request is creating user with unique data which wasn't used before,
-         * therefore it is valid and should return 201 Created. We test response code of first request just in case something
-         * goes wrong, even though this request isn't the subject of our test.
-         * */
-        this.mockMvc.perform(post("/api/v1/users")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(json))
-                .andExpect(status().isCreated());
+
+        createUser(user);
 
         /*
          * Second request is creating user with unique data which was used before (user created in previous request),
@@ -113,24 +133,12 @@ public class WebLayerTest {
         User user = new User("user");
         user.setId(userId);
 
-        // convert to JSON
-        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
-        String json = ow.writeValueAsString(user);
+        createUser(user);
 
-        /*
-         * First request is creating user with unique data which wasn't used before,
-         * therefore it is valid and should return 201 Created. We test response code of first request just in case something
-         * goes wrong, even though this request isn't the subject of our test.
-         * */
-        this.mockMvc.perform(post("/api/v1/users")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(json))
-                .andExpect(status().isCreated());
 
         /*
          * This request is getting data of user created in previous request. It is valid and should return 200 OK.
          */
-
         MvcResult result = this.mockMvc.perform(get("/api/v1/users/" + userId))
                 .andExpect(status().isOk())
                 .andReturn();
@@ -152,25 +160,12 @@ public class WebLayerTest {
         User user = new User(username);
         user.setId(userId);
 
-        // convert to JSON
-        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
-        String json = ow.writeValueAsString(user);
-
-        /*
-         * First request is creating user with unique data which wasn't used before,
-         * therefore it is valid and should return 201 Created. We test response code of first request just in case something
-         * goes wrong, even though this request isn't the subject of our test.
-         * */
-        this.mockMvc.perform(post("/api/v1/users")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(json))
-                .andExpect(status().isCreated());
-
+        createUser(user);
 
         /* Create new JSON with updated ID */
         String newUserId = "changedUserId";
         user.setId(newUserId);
-        json = ow.writeValueAsString(user);
+        String json = objectToJson(user);
 
 
         /* Send PUT request on /api/v1/user/oldId
@@ -192,5 +187,61 @@ public class WebLayerTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content("{}")
         ).andExpect(status().isNotFound());
+    }
+
+    @Test
+    void updateUserShouldReturnHTTPOKOnValidUserUpdate() throws Exception{
+        String userId = "userId";
+        String username = "user";
+
+        User user = new User(username);
+        user.setId(userId);
+
+        /* convert to JSON */
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        String json = ow.writeValueAsString(user);
+
+        /*
+         * First request is creating user with unique data which wasn't used before,
+         * therefore it is valid and should return 201 Created. We test response code of first request just in case something
+         * goes wrong, even though this request isn't the subject of our test.
+         * */
+        this.mockMvc.perform(post("/api/v1/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isCreated());
+
+
+        /* Update User's username and update JSON */
+        String changedUsername = "changedUsername";
+        user.setUsername(changedUsername);
+
+        json = ow.writeValueAsString(user);
+
+        /* Send PUT request with ID, changed username in request content */
+        this.mockMvc.perform(put("/api/v1/users/" + userId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json)
+        ).andExpect(status().isOk());
+    }
+
+    /* ID in body is not required.
+    * If there is no ID in body, value from URI should be used
+    */
+    @Test
+    void updateUserShouldReturnHTTPOKWhenUserIDIsNotInBodyAndRestIsValid() throws Exception{
+        String username = "user";
+        String userId = "userId";
+
+        User user = new User(username);
+        user.setId(userId);
+
+        createUser(user);
+
+        String changedUsername = "changedUsername";
+        this.mockMvc.perform(put("/api/v1/users/" + userId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"username\": \"" + changedUsername + "\"}")
+        ).andExpect(status().isOk());
     }
 }
